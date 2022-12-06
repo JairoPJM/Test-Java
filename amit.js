092621849600c3c3a0de6ae0cc90acaa44cad5c3
Minjae Cho <113135811+slchld1@users.noreply.github.com>
2022-12-05 20:03:58 -0500

    lan-lon

--- amit.js
+++ amit.js
@@ -21,3 +21,52 @@
 
 //     }
 // }
+
+// var btn = document.getElementById('click')
+
+// function getAPI() {
+//     $.ajaxSetup({
+//         url: "https://api.walkscore.com" + ajax.data
+//     });
+//     $.ajax({data: {
+//         address : '1119 8th Ave S, Seattle, WA,',
+//         lat : '47.6085',
+//         lon : '-122.3295',
+//         wsapikey : 'abda595364afb8f553e850e39711ae84',
+//     }})
+
+// fetch(url, {
+//     method: 'GET',
+//     mode: 'no-cors',
+//     credentials: 'same-origin',
+// })
+// .then(function (response) {
+//     return response.json()
+// })
+// .then(function (data) {
+//     console.log(data);
+// });
+// }
+// btn.addEventListener('click', getAPI)
+$('#form').submit( function(e) {
+    e.preventDefault();
+    var input = $('#search').val();
+    var inputArr = input.split(',')
+    console.log(inputArr)
+
+
+function getLocation(city,state) {
+    var key = 'b5d6abd64c1bcaf907e06b633fed6528';
+    var city = inputArr[0];
+    var state = inputArr[1];
+    var country = '';
+    fetch('http://api.openweathermap.org/geo/1.0/direct?q='+city+','+ state +','+country + '&limit=5&appid=' + key)
+    .then(function(resp) { return resp.json() })
+    .then(function(data) {
+        console.log(data)
+    })
+    .catch(function(){
+    });
+}
+getLocation();
+});
