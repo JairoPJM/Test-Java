9ee92bfd40cce3aa9bb39da378e669141db8186e
unknown <jaecho203@gmail.com>
2022-12-08 19:45:31 -0500

    map is added to switch current location to desired location

--- amit.js
+++ amit.js
@@ -16,6 +16,14 @@ function getLocation(city,state) {
         console.log(data)
         console.log(data[0].lat)
         console.log(data[0].lon)
+        let map
+        function initMap() {
+            map = new google.maps.Map(document.getElementById("map"), {
+                center: { lat: data[0].lat, lng: data[0].lon},
+                zoom:12,
+            });
+        }
+        window.initMap = initMap();
         function getCrime() {
             const options = {
                 method: 'GET',
@@ -28,45 +36,29 @@ function getLocation(city,state) {
             var long = data[0].lon
             fetch('https://api.crimeometer.com/v1/incidents/raw-data?lat='+lat+'&lon='+long+'&distance=10mi&datetime_ini=2020-01-01T11:33:54.000Z&datetime_end=2022-12-01T11:33:54.000Z&page=1',options)
                 .then(response => response.json())
-                .then(response => console.log(response))
-                .catch(err => console.error(err));
+                .then(response => {
+                    console.log(response)
+                    let crimeData;
+                    response = crimeData;
+                    
+                    for(let i = 0; i < (crimeData.incidents.length); i++){
+                    
+                    var myLatlng = new google.maps.LatLng(crimeData.incidents[i].incident_latitude,crimeData.incidents[i].incident_longitude);
+                    var marker = new google.maps.Marker({
+                        position: myLatlng,
+                        title: crimeData.incidents[i].incident_offense
+                    })
+                    marker.setMap(map);
+                }
+                    
+                    // getMarker();
+                    })
+                    .catch(err => console.error(err));
+                }
+                getCrime();
+            })
+            .catch(function() {
+            })
         }
-getCrime();
-})
-.catch(function() {
-})
-}
-getLocation()
+        getLocation()
 });
-// function getWalkScore() {
-//     const options = {
-//         method: 'GET',
-//         headers: {
-//             'X-RapidAPI-Key': '3b327d0059msha07f2e8b5e08905p13aff9jsna9d2b3f3d9f3',
-//             'X-RapidAPI-Host': 'walk-score.p.rapidapi.com'
-//         }
-//     };
-//     var lat = data[0].lat
-//     var lon = data[0].lon
-//     var key = 'abda595364afb8f553e850e39711ae84'
-//     var address = inputArr[0].trim()+'%2C%20' + inputArr[1].trim()
-//     fetch('https://walk-score.p.rapidapi.com/score?lat=' + lat +'&address='+ address + '&wsapikey='+ key + '&lon='+ lon, options)
-//     .then(response => response.text())
-//     .then(data => {
-//         console.log(data)
-//         var dataJSON = parseFloat(data)
-//         console.log(dataJSON.walkScore)
-//     })
-//     .catch(err => console.error(err));
-// }
-// getWalkScore();
-let map;
-
-function initMap() {
-map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 33.7489924, lng: -84.3902644 },
-    zoom:12,
-});
-}
-
-window.initMap = initMap;
