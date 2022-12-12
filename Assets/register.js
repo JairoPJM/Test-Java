9bb70e650dc20134f1bccb80a736d815c39cddcd
brooksteven <brooksteven580@gmail.com>
2022-12-12 11:58:24 -0500

    added comments to regiter.js and signup.js

--- Assets/register.js
+++ Assets/register.js
@@ -1,14 +1,19 @@
+// Start function to register
 function signUp(e){
     console.log("signUp")
     event.preventDefault();
 
+//Returnring element by id that matches the specified string
 var name = document.getElementById('name').value;
 var email = document.getElementById('email-signup').value;
 var password = document.getElementById('password-signup').value;
+//Function to print information to the console
+//The easiest way to view the output of a string is to print it to the console, with console.log()
 console.log("name = ", name);
 console.log("email = ", email);
 console.log("password = ", password);
 
+// User input variables:
 var user = { 
     name: name,
     email: email,
@@ -19,12 +24,17 @@ var json = JSON.stringify(user);
 localStorage.setItem(email, json);
 console.log('user added');
 
-
+//If statement for user validation/registration 
 if(name=="admin" && email=="admin@gmail.com" && password=="admin123"){
+    //The window will load and display the index.html document
     window.location.assign("index.html");
+    // Validates user input
+    // Start user input prompts
     alert("Login Successful");
 }
 else{
+    // Validates user input
+    // Start user input prompts
     alert("Invalid Information");
     return;
 }
@@ -32,12 +42,12 @@ else{
 
 
 
-// Declared variables
+// Declared variable
 var signUp1 = document.querySelector("#signUp");
 
 
 
-// Retreives local stroage 
+// Retrives local stroage 
 var userInfo = localStorage.getItem("userInfo");
 userInfo = JSON.parse(userInfo);
 
--- Assets/signup.js
+++ Assets/signup.js
@@ -1,14 +1,13 @@
-//Sign up Function
-
+// Start function to Login
 function signIn(e){
     console.log("signIn")
     event.preventDefault();
 
-
+//Returnring element by id that matches the specified string
 var email = document.getElementById('email-login').value;
 var password = document.getElementById('password-login').value;
 
-
+// User input variables:
 var user = {
     email: email,
     password: password,
@@ -18,12 +17,17 @@ var json = JSON.stringify(user);
 localStorage.setItem(email, json);
 console.log('user added');
 
-
+// if statement for user validation/login 
 if(email=="admin@gmail.com" && password=="admin123"){
+    //The window will load and display the index.html document
     window.location.assign("index.html");
+     // Validates user input
+    // Start user input prompts
     alert("Login Successful");
 }
 else{
+    // Validates user input
+    // Start user input prompts
     alert("Invalid Information");
     return;
 }
